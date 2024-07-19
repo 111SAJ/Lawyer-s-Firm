bba89291ee1e0d354e3a3f92a9fd9dea13d74087
111SAJ <147030794+111SAJ@users.noreply.github.com>
2024-07-19 16:48:15 +0530

    scroll up, whats app icon added
    working

--- UI/JavaScript.js
+++ UI/JavaScript.js
@@ -38,3 +38,26 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// scroll up starts
+// Get the button
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+// scroll up ends
